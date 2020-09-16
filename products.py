a41f4b035b06be51996a357c38f8f10a684180a3
Peyton Ko <colvin17@gmail.com>
2020-09-16 22:14:56 +0800

    add for loop

--- products.py
+++ products.py
@@ -16,5 +16,9 @@ while True: # while loop 比for loop適合用在這裡，因為不知道會輸�
     products.append([name, price])
 
 print(products)
-
 products[0][0] # 讀取二維清單，第一個[0]是product清單的第0格位置，第二個[0]是p清單的第0個位置。即先讀大清單的位置再讀小清單的位置。
+
+for p in products:
+    print(p) # 單獨印出products每項物品資訊(name & price)直到印完所有物品。
+    print(p[0]) # 只印出商品名字。
+    print(p[0], '的價格是', p[1], '元。')
