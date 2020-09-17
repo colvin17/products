38cea6cef5841f720046b413143f4754cddb9bee
Peyton Ko <colvin17@gmail.com>
2020-09-17 22:38:54 +0800

    add read file

--- products.py
+++ products.py
@@ -1,6 +1,14 @@
 # 記帳程式
-
+# 讀取檔案
 products = []
+with open('products.csv', 'r', encoding = 'utf-8') as f:
+    for line in f:
+        if '商品,價格' in line:
+            continue # 跳過抬頭，通常寫在迴圈中很高的位置。continue和break只能寫在迴圈裡。
+        name, price = line.strip().split(',') # 讀csv檔去掉換行或空格符號後看到逗點就分割資料，並存到name跟price清單。
+        products.append([name, price])
+
+# 讓使用者輸入
 while True: # while loop 比for loop適合用在這裡，因為不知道會輸入幾項記帳物品。
     name = input('請輸入商品名稱:')
     if name == 'q':
@@ -19,16 +27,18 @@ while True: # while loop 比for loop適合用在這裡，因為不知道會輸�
 print(products)
 products[0][0] # 讀取二維清單，第一個[0]是product清單的第0格位置，第二個[0]是p清單的第0個位置。即先讀大清單的位置再讀小清單的位置。
 
+# 印出所有購買紀錄
 for p in products:
     print(p) # 單獨印出products每項物品資訊(name & price)直到印完所有物品。
     print(p[0]) # 只印出商品名字。
     print(p[0], '的價格是', p[1], '元。')
 
+# 寫入檔案
 # 字串可以加、乘，無法減、除
 # 'abc' + '123' = 'abc123'
 # 'abc' * 3 = 'abcabcabc' #很少用到
 with open('products.csv', 'w', encoding = 'utf-8') as f: # 加入編碼格式避免印出亂碼
-	f.write('商品,價格\n') 
+    f.write('商品,價格\n') 
     for p in products:
         f.write(p[0] + ',' + str(p[1]) + '\n') # \n換行, 中間加逗點方便csv檔用excel打開時自動將商品名字及價錢分成兩個儲存格。
         # 字串無法和整數相加，所以要將price再從int轉換成str。
